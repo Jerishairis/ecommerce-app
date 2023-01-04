0b8882ad6219162614ab088b4fd64e99871cb805
Jerishairis <Bugnonjerishairis@gmail.com>
2023-01-04 22:05:02 +0800

    update ordercontroller

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require('../auth.js');
+const oderController = require("../controller/orderController.js");
 
 
 router.post("/new-order", auth.verify, (req, res) => {
